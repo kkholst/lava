661cc41694b41241530a6453ba3a432400483461
Klaus Holst <klaus@holst.it>
2012-05-14 12:05:25 +0000

    Removing usage of unlockBinding
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/lava@274 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/hooks.R
+++ R/hooks.R
@@ -25,55 +25,63 @@
 ##' @export
 lava.options <- function(...) {
   dots <- list(...)
-  curopt <- get("options",envir=lava.env)
+  lavaopt <- options()$lava
+  curopt <- lavaopt$options
+  ##  curopt <- get("options")##,envir=lava.env)
   if (length(dots)==0) 
     return(curopt)
-  unlockBinding("options",lava.env)
+  ##  unlockBinding("options",lava.env)
   idx <- which(names(dots)!="")
   curopt[names(dots)[idx]] <- dots[idx]
-  assign("options",curopt, envir=lava.env)  
-  lockBinding("options", lava.env)  
+  ##  assign("options",curopt,) ##, envir=lava.env)
+  lavaopt$options <- curopt
+  options(lava=lavaopt)
+  invisible(curopt)
+  ##  lockBinding("options", lava.env)  
 }
 
 ##' @export
 gethook <- function(hook="estimate.hooks",...) {
-  get(hook,envir=lava.env)
+  options()$lava[[hook]]
+  ##  get(hook,envir=lava.env)
 }
 
 ##' @export
 addhook <- function(x,hook="estimate.hooks",...) {
   ##  env <- as.environment("package:lava")
-  unlockBinding(hook,lava.env)
-  assign(hook,unique(c(gethook(hook),x)), envir=lava.env)
-  lockBinding(hook, lava.env)
+  ##  unlockBinding(hook,lava.env)
+  ##  assign(hook,unique(c(gethook(hook),x)),parent.frame(1))##, envir=lava.env)
+  lavaopt <- options()$lava
+  lavaopt[[hook]] <- c(gethook(hook),x)
+  options(lava=lavaopt)
+  ##  lockBinding(hook, lava.env)
 }
 
-lava.env <- environment()
-assign("init.hooks",c(),lava.env)
-lockBinding("init.hooks", lava.env)
-assign("estimate.hooks",c(),lava.env)
-lockBinding("estimate.hooks", lava.env)
-assign("color.hooks",c(),lava.env)
-lockBinding("color.hooks", lava.env)
-assign("sim.hooks",c(),lava.env)
-lockBinding("sim.hooks", lava.env)
-assign("post.hooks",c(),lava.env)
-lockBinding("post.hooks", lava.env)
-assign("options",
-       list(
-            trace=0,
-            iter.max=300,
-            eval.max=250,
-            constrain=FALSE,
-            silent=TRUE,            
-            itol=1e-9,
-            Dmethod="simple", ##Richardson"
-            parallel=TRUE,
-            param="relative",
-            constrain=TRUE,
-            exogenous=TRUE,
-            Rgraphviz=TRUE,
-            debug=FALSE
-            ),
-       lava.env)
-
+## ## ##lockBinding("init.hooks", lava.env)
+## ## assign("estimate.hooks",c(),lava.env)
+## ## ##lockBinding("estimate.hooks", lava.env)
+## ## assign("color.hooks",c(),lava.env)
+## ## ##lockBinding("color.hooks", lava.env)
+## ## assign("sim.hooks",c(),lava.env)
+## ## ##lockBinding("sim.hooks", lava.env)
+## ## assign("post.hooks",c(),lava.env)
+## ## ##lockBinding("post.hooks", lava.env)
+##           options=
+##           ## assign("options",
+##           list(
+##             trace=0,
+##             iter.max=300,
+##             eval.max=250,
+##             constrain=FALSE,
+##             silent=TRUE,            
+##             itol=1e-9,
+##             Dmethod="simple", ##Richardson"
+##             parallel=TRUE,
+##             param="relative",
+##             constrain=TRUE,
+##             exogenous=TRUE,
+##             Rgraphviz=TRUE,
+##             debug=FALSE
+##             )))
+##)
+##       lava.env)
