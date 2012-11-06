1de111656d1642c521db996e0c3d6fd2b377771f
Klaus Holst <klaus@holst.it>
2012-11-06 09:04:28 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/lava@414 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/utils.R
+++ R/utils.R
@@ -209,7 +209,8 @@ fixsome <- function(x, exo.fix=TRUE, measurement.fix=TRUE, S, mu, n, data, x0=FA
             if (all(is.na(x$fix[e, ]==1)) &
                 is.na(x$covpar[e,e]) & is.na(x$covfix[e,e])) 
               regfix(x,from=e,to=ys.[1]) <- 1
-            if (is.na(intercept(x)[[ys.[1]]]) &
+            if (!any(unlist(lapply(intercept(x)[ys],is.numeric))) &
+                ##is.na(intercept(x)[[ys.[1]]]) &
                 is.na(intercept(x)[[e]]))
               intercept(x,ys.[1]) <- 0
           }
