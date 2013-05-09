a7897c5a8b73204ab84674279ff1c4523a34801f
Klaus Holst <klaus@holst.it>
2013-05-09 20:42:22 +0000

    man+tests updated
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/lava@659 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/tests/test-multigroup.R
+++ inst/tests/test-multigroup.R
@@ -56,42 +56,6 @@ test_that("Multiple group, missing data analysis", {
 
 
 
-test_that("Multiple group constraints", {
-  data(twindata)
-  twinwide <- reshape(twindata,direction="wide",
-                      idvar="id",timevar="twinnum")
-  l <- lvm(~bw.1+bw.2)
-  covariance(l) <- bw.1 ~ bw.2
-  e <- estimate(l,subset(twinwide,zyg.1=="MZ"))
-  B <- cbind(1,-1); colnames(B) <- c("bw.1<->bw.1","bw.2<->bw.2")
-  lava::compare(e,contrast=B)
-  B2 <- rbind(c(1,-1,0,0),c(0,0,1,-1))
-  colnames(B2) <- c("bw.1","bw.2","bw.1<->bw.1","bw.2<->bw.2")
-  lava::compare(e,contrast=B2)
-
-  l <- lvm(~bw.1+bw.2)
-  covariance(l) <- bw.1 ~ bw.2
-  intercept(l,~bw.1+bw.2) <- "m"
-  covariance(l,~bw.1+bw.2) <- "s"
-  covariance(l,bw.1~bw.2) <- "r1"
-  l2 <- l
-  covariance(l2,bw.1~bw.2) <- "r2"
-
-  DZ <- subset(twinwide,zyg.1=="MZ")
-  MZ <- subset(twinwide,zyg.1=="DZ")
-  e <- estimate(l,MZ)
-  e2 <- estimate(l2,DZ)
-
-  parameter(l) <- ~r2
-  parameter(l2) <- ~r1
-  ee <- estimate(list(MZ=l,DZ=l2),list(MZ,DZ))
-  constrain(ee,h~r1+r2) <- function(x) 2*(x[1]-x[2])
-  ce <- constraints(ee)
-  expect_true(length(coef(ee))==4)
-  expect_true(nrow(ce)==1)
-  expect_true(all(!is.na(ce)))
-  expect_true(sum(score(ee)^2)<1e-4)
-}
 
 
 
