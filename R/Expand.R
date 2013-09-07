870a46f4f21bb0d433542e80249212373d9f9669
Klaus Holst <klaus@holst.it>
2013-09-07 16:23:18 +0000

    Expand now also works with tables. estimate id bug-fix
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/lava@719 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/Expand.R
+++ R/Expand.R
@@ -1,6 +1,8 @@
 ##' Create a Data Frame from All Combinations of Factors
 ##'
-##' Simple wrapper of the 'expand.grid' function
+##' Simple wrapper of the 'expand.grid' function.  If x is a table
+##' then a data frame is returned with one row pr individual
+##' observation. 
 ##' @title Create a Data Frame from All Combinations of Factors
 ##' @param x Data.frame
 ##' @param ... vectors, factors or a list containing these
@@ -9,7 +11,15 @@
 ##' @examples
 ##' dd <- Expand(iris, Sepal.Length=2:8, Species="virginica")
 ##' str(dd)
+##'
+##' T <- with(warpbreaks, table(wool, tension))
+##' Expand(T)
 Expand <- function(x,...) {
+    if (inherits(x,"table")) {
+        M <- as.data.frame(x)
+        idx <- rep(seq(nrow(M)),M[,ncol(M)])
+        return(M[idx,-ncol(M),drop=FALSE])                
+    }
     dots <- list(...)
     nn <- names(dots)
     for (n in nn) {
