8d0ef9afb02b1d8aa937745ad7f238b15372288b
Klaus Holst <klaus@holst.it>
2013-10-23 13:17:56 +0000

    NR algorithm with back-tracking (step-size halfed until score is reduced in 2-norm)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/lava@739 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/optims.R
+++ R/optims.R
@@ -173,4 +173,107 @@ NR <- function(start,objective,gradient,hessian,debug=FALSE,control,...) {
     return(res)
 }
 
+###}}} NR
+
+###{{{ NR 2
+
+NR <- function(start,objective,gradient,hessian,debug=FALSE,control,...) {
+    control0 <- list(trace=0,gamma=1,lambda=0,ngamma=0,gamma2=0,
+                     iter.max=200,tol=1e-15,stabil=FALSE,epsilon=1e-15)
+    if (!missing(control)) {
+        control0[names(control)] <- control
+    }
+    trace <- control0$trace
+
+    if (trace>0)
+        cat("\nIter=0;\t\n",
+            "\tp=", paste(formatC(start), collapse=" "),"\n",sep="")
+
+    gradFun = !missing(gradient)    
+    if (!gradFun & missing(hessian)) {
+        hessian <- function(p) {
+            ff <- objective(p)
+            res <- attributes(ff)$hessian
+            attributes(res)$grad <- as.vector(attributes(ff)$grad)
+            return(res)
+        }
+    }
+    oneiter <- function(p.orig,return.mat=FALSE) {
+        if (is.null(hessian)) {
+            cat(".")
+            I <- -numDeriv::jacobian(gradient,p.orig,method=lava.options()$Dmethod)
+        } else {
+            I <- -hessian(p.orig)
+        }
+        D <- attributes(I)$grad
+        if (is.null(D)) {      
+            D <- gradient(p.orig)
+        }
+        if (return.mat) return(list(D=D,I=I))
+        if (control0$stabil) {
+            if (control0$lambda!=0) {
+                if (control0$lambda<0) {
+                    sigma <- (t(D)%*%(D))[1]          
+                } else {
+                    sigma <- control0$lambda
+                }
+                sigma <- min(sigma,10)
+                I <- I+control0$gamma2*sigma*diag(nrow(I))
+            } else {
+                sigma <- ((D)%*%t(D))
+                I <- I+control0$gamma2*(sigma)
+            }
+        }
+        svdI <- svd(I); svdI$d0 <- numeric(length(svdI$d));
+        svdI$d0[abs(svdI$d)>control0$epsilon] <-
+            1/svdI$d[abs(svdI$d)>control0$epsilon]
+        iI <- with(svdI,  (v)%*%diag(d0,nrow=length(d0))%*%t(u))
+        Delta = control0$gamma*iI%*%D
+        mD = mD0 = mean(D^2)
+        Lambda <- 1
+        while (mD>=mD0) {
+            p <- p.orig + Lambda*Delta
+            if (gradFun) {
+                D = gradient(p)                
+            } else {
+                DI <- oneiter(p,return.mat=TRUE)
+                D = DI$D
+            }
+            mD = mean(D^2)
+            if (is.nan(mD)) mD=mD0
+            Lambda <- Lambda/2
+            if (Lambda<1e-12) break;
+        }        
+        return(list(p=p,D=D,iI=iI))
+    } 
+    
+    count <- count2 <- 0  
+    thetacur <- start
+    gammacount <- 0
+    for (jj in seq_len(control0$iter.max)) {
+        gammacount <- gammacount+1
+        count <-  count+1
+        count2 <- count2+1
+        oldpar <- thetacur
+        newpar <- oneiter(thetacur)
+        thetacur <- newpar$p
+        if (!is.null(control0$ngamma) && control0$ngamma>0) {
+            if (control0$ngamma<=gammacount) {
+                control0$gamma <- sqrt(control0$gamma)
+                gammacount <- 0
+            }
+        }
+        if (count2==trace) {
+            cat("Iter=",count, ";\n\tD=", paste(formatC(newpar$D), collapse=" "),"\n",sep="")
+            cat("\tp=", paste(formatC(thetacur), collapse=" "),"\n",sep="")      
+            count2 <- 0
+        }
+        if (mean(newpar$D^2)<control0$tol) break;
+    }
+    res <- list(par=as.vector(thetacur), iterations=count, method="NR",
+                gradient=newpar$D, iH=newpar$iI)
+    return(res)
+}
+
+
 ###}}} Newton Raphson/Scoring
