e04368e9409f993b405256453f6b31fed00aa9a2
Klaus Holst <klaus@holst.it>
2014-02-17 13:11:33 +0000

    forrestplot added
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/lava@925 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zconfband.R
+++ R/zconfband.R
@@ -17,44 +17,87 @@
 ##' @seealso \code{confband}
 ##' @export
 ##' @keywords iplot
+##' @aliases confband forrestplot
 ##' @examples
 ##' plot(0,0,type="n",xlab="",ylab="")
 ##' confband(0.5,-0.5,0.5,0,col="darkblue")
 ##' confband(0.8,-0.5,0.5,0,col="darkred",vert=FALSE,pch=1,cex=1.5)
+##' 
+##' set.seed(1)
+##' K <- 20
+##' est <- rnorm(K); est[c(3:4,10:12)] <- NA
+##' se <- runif(K,0.2,0.4)
+##' x <- cbind(est,est-2*se,est+2*se,runif(K,0.5,2))
+##' rownames(x) <- unlist(lapply(letters[seq(K)],function(x) paste(rep(x,4),collapse="")))
+##' signif <- sign(x[,2])==sign(x[,3])
+##' forrestplot(x,sep=c(2,15),col=signif+1)
 ##' @author Klaus K. Holst
 confband <- function(x,lower,upper,center=NULL,delta=0.07,centermark=0.03,
                      pch,blank=TRUE,vert=TRUE,...) {
-  if (vert) {
-    if (!is.null(lower)) {
-      segments(x,lower,x,upper,...)
-      segments(x-delta,lower,x+delta,lower,...)
-      segments(x-delta,upper,x+delta,upper,...)
-    }
-    if (!is.null(center)) {
-      if (!missing(pch)) {
-        if (blank)
-          points(x,center,pch=16,col="white")
-        points(x,center,pch=pch,...)
-      } else {
-        segments(x-centermark,center,x+centermark,center,...)
-      }
-    }
-  } else {
-  
-      if (!is.null(lower)) {
-        segments(lower,x,upper,x,...)
-        segments(lower,x-delta,lower,x+delta,...)
-        segments(upper,x-delta,upper,x+delta,...)
-      }
-      if (!is.null(center)) {
-        if (!missing(pch)) {
-          if (blank)
-        points(center,x,pch=16,col="white")
-          points(center,x,pch=pch,...)
-        } else {
-          segments(center,x-centermark,center,x+centermark,...)
+    if (vert) {
+        if (!is.null(lower)) {
+            segments(x,lower,x,upper,...)
+            segments(x-delta,lower,x+delta,lower,...)
+            segments(x-delta,upper,x+delta,upper,...)
         }
-      }
+        if (!is.null(center)) {
+            if (!missing(pch)) {
+                if (blank)
+                    points(x,center,pch=16,col="white")
+                points(x,center,pch=pch,...)
+            } else {
+                segments(x-centermark,center,x+centermark,center,...)
+            }
+        }
+    } else {
+        
+        if (!is.null(lower)) {
+            segments(lower,x,upper,x,...)
+            segments(lower,x-delta,lower,x+delta,...)
+            segments(upper,x-delta,upper,x+delta,...)
+        }
+        if (!is.null(center)) {
+            if (!missing(pch)) {
+                if (blank)
+                    points(center,x,pch=16,col="white")
+                points(center,x,pch=pch,...)
+            } else {
+                segments(center,x-centermark,center,x+centermark,...)
+            }
+        }
     }
     invisible(c(x,lower,upper,center))
 }
+
+
+##' @export
+forrestplot <- function(x,lower,upper,vline=0,labels,text=TRUE,axes=TRUE,cex=1,pch=15,xlab="",ylab="",sep,delta,xlim,ylim,mar=c(4,8,1,1),...) {
+    if (is.matrix(x)) {
+        lower <- x[,2]; upper <- x[,3]
+        if (ncol(x)>3) cex <- x[,4]
+        x <- x[,1]
+    }
+    if (missing(labels)) labels <- names(x)    
+    K <- length(x)
+    op <- par(mar=mar) ## bottom,left,top,right
+    on.exit(par(op))
+    if (missing(ylim)) ylim <- c(1,K)
+    if (missing(xlim)) {
+        if (missing(delta)) delta <- max(upper-lower,na.rm=TRUE)*0.4
+        xlim <- range(c(x,lower-delta,upper+delta),na.rm=TRUE)
+    }
+    plot(0,type="n",axes=FALSE,xlab=xlab,ylab=ylab,xlim=xlim,ylim=ylim,...)
+    if (axes) axis(1)
+    if (!is.null(vline)) abline(v=vline,lty=2,col="lightgray")
+    if (!missing(sep)) abline(h=sep+.5,col="gray")
+    confband(seq(K),lower,upper,x,pch=pch,cex=cex,vert=FALSE,delta=0,blank=FALSE,...)
+    mtext(labels,2,at=seq(K),las=2)
+    if (text)
+        for (i in seq_len(K)) {
+            st <- paste(formatC(x[i])," (",formatC(lower[i]),";",formatC(upper[i]),")",sep="")
+            graphics::text(upper[i],i,st,xpd=TRUE,pos=4,cex=0.6)
+        }
+}
+
+
+
