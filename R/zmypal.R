4cd1470987363d3b588e0af44e99a1453fd9e3d3
Klaus Holst <klaus@holst.it>
2014-02-17 13:10:52 +0000

    returns old-palette
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/lava@906 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zmypal.R
+++ R/zmypal.R
@@ -1,8 +1,10 @@
 mypal <- function(set=TRUE,...) {
-  col <- c("black","darkblue","darkred","goldenrod","mediumpurple",
-  "seagreen","aquamarine3","violetred1","salmon1",
-  "lightgoldenrod1","darkorange2","firebrick1","violetred1", "gold")
-  if (!set) return(col)
-  palette(col)
+    oldpal <- palette()
+    col <- c("black","darkblue","darkred","goldenrod","mediumpurple",
+             "seagreen","aquamarine3","violetred1","salmon1",
+             "lightgoldenrod1","darkorange2","firebrick1","violetred1", "gold")
+    if (!set) return(col)
+    palette(col)
+    invisible(oldpal)
 }
                          
