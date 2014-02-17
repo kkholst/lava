36977be9e367c7f4da9e847a17ea5efed7d07d89
Klaus Holst <klaus@holst.it>
2014-02-17 13:11:42 +0000

    Update fixed expars
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/lava@929 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/parameter.R
+++ R/parameter.R
@@ -7,15 +7,10 @@
   return(x)
 }
 
+
 ##' @S3method parameter<- lvm
-"parameter<-.lvm" <- function(x,start,...,value) {
-  if (class(value)[1]=="formula") {
-    parameter(x,...) <- all.vars(value)
-    return(x)
-  }
-  ## latent(x,silent=TRUE) <- value
-  ## covfix(x,value,NULL) <- 1
-  ## intfix(x, value) <- value
+"parameter<-.lvm" <- function(x,constrain,start,...,value) {
+  if (class(value)[1]=="formula") value <- all.vars(value)
   if (!missing(start)) {
       if (length(start) != length(value)) stop("'start' and 'value' should be of the same lengths")
       start <- as.list(start)
@@ -23,16 +18,22 @@
   } else {
       start <- as.list(rep(0,length(value))); names(start) <- value
   }
-  newfix <- as.list(value); names(newfix) <- value
+  if (!missing(constrain)) {      
+      newfix <- constrain
+      if (!is.list(newfix)) newfix <- as.list(newfix)
+  } else {
+      newfix <- as.list(value);
+  }
+  names(newfix) <- value
   x$expar[value] <- start
   x$exfix[value] <- newfix
-  ##x$expar <- c(x$expar,start)
-  ##x$exfix <- c(x$exfix,newfix)
   index(x) <- reindex(x)
   x$attributes$parameter[value] <- TRUE
   return(x)
 }
 
 ##' @export
-parameter <- function(x,...)
-    names(unlist(x$attributes$parameter))
+parameter <- function(x,var,...) {
+    if (missing(var)) return (names(unlist(x$attributes$parameter)))
+    parameter(x,...) <- var
+}
