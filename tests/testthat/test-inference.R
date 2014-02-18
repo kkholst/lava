5b3b22bf8ab843b3050d56654d078d57a7da57b6
Klaus Holst <klaus@holst.it>
2014-02-18 11:27:33 +0000

    unit testing (closed-form estimators)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/lava@962 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/testthat/test-inference.R
+++ tests/testthat/test-inference.R
@@ -29,3 +29,26 @@ test_that("Profile confidence limits", {
     ci <- confint(e,3,profile=TRUE)
     expect_true(mean((ci0-ci)^2)<0.1)
 }
+
+test_that("IV-estimator", {
+    m <- lvm(c(y1,y2,y3)~u); latent(m) <- ~u    
+    set.seed(1)
+    d <- sim(m,100)
+    e0 <- estimate(m,d)
+    e <- estimate(m,d,estimator="iv") ## := MLE
+    expect_true(mean((coef(e)-coef(e0))^2)<1e-9)
+})
+
+test_that("glm-estimator", {         
+    m <- lvm(y~x+z)
+    regression(m) <- x~z
+    distribution(m,~y+z) <- binomial.lvm("logit")
+    set.seed(1)
+    d <- sim(m,1e3)
+    head(d)
+    e <- estimate(m,d,estimator="glm")
+    c1 <- coef(e,2)[c("y","y~x","y~z"),1:2]
+    c2 <- coef(summary(glm(y~x+z,d,family=binomial)))[,1:2]  
+    expect_equivalent(c1,c2)
+})
+
