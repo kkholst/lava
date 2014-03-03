bd80ed30f0a94e6b8e9b4c89a7a2b70842fe98b8
Klaus Holst <klaus@holst.it>
2014-03-03 15:21:36 +0000

    ordinal.color hook
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/lava@1004 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/onload.R
+++ R/onload.R
@@ -3,6 +3,8 @@
     addhook("heavytail.init.hook","init.hooks")
     addhook("glm.estimate.hook","estimate.hooks")
     addhook("cluster.post.hook","post.hooks")
+    addhook("ordinal.sim.hook","sim.hooks")
+    addhook("color.ordinal","color.hooks")
     
     desc <- packageDescription(pkg)
     ## packageStartupMessage("Loading '", desc$Package, "' package...\n",
