17e66baf87ae1f5d92761ab73ddccce1244cd5f9
Thomas Gerds <tag@biostat.ku.dk>
2014-03-14 12:52:33 +0000

    rename of ID function to click method
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/lava@1032 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zId.R
+++ R/zId.R
@@ -6,37 +6,43 @@
 ##' the pop-up menu equivalent (usually second mouse button or
 ##' 'Ctrl'-click) or the 'ESC' key.
 ##' @title Identify points on plot
-##' @usage
-##' Id(x, y=NULL, label=TRUE, n=length(x), pch=19, col="orange", cex=3, ...)
+##' @usage 
+##' \method{click}{default}(x, y=NULL, label=TRUE, n=length(x), pch=19, col="orange", cex=3, ...)
 ##' 
 ##' idplot(x,y,...,id=list())
 ##' @aliases idplot
-##' @param x X coordinates
-##' @param y Y coordinates
-##' @param label Should labels be added?
-##' @param n Max number of inputs to expect
-##' @param pch Symbol
-##' @param col Color
-##' @param cex Size
-##' @param id List of arguments parsed to \code{Id} function
-##' @param \dots Additional arguments parsed to \code{plot} function
+#' @param object
+#' @param ...
+#' @param #x X coordinates
+#' @param #y Y coordinates
+#' @param #label Should labels be added?
+#' @param #n Max number of inputs to expect
+#' @param #pch Symbol
+#' @param #col Color
+#' @param #cex Size
+#' @param #id List of arguments parsed to \code{Id} function
+#' @param #\dots Additional arguments parsed to \code{plot} function
 ##' @author Klaus K. Holst
 ##' @seealso \code{\link{idplot}}, \code{identify}
-##' @export
 ##' @examples
 ##' \donttest{
 ##' n <- 10; x <- seq(1:n); y <- runif(n)
-##' plot(y ~ x); Id(x,y)
+##' plot(y ~ x); click(x,y)
 ##' 
 ##' data(iris)
 ##' l <- lm(Sepal.Length ~ Sepal.Width*Species,iris)
 ##' res <- plotConf(l,var2="Species")## ylim=c(6,8), xlim=c(2.5,3.3))
-##' with(res, Id(x,y))
+##' with(res, click(x,y))
 ##' 
 ##' with(iris, idplot(Sepal.Length,Petal.Length))
 ##' }
 ##' @keywords iplot
-`Id` <-
+#' @export
+click <- function(x,...){
+    UseMethod("click")
+}
+##' @S3method click default
+click.default <-
 function(x, y=NULL, label=TRUE, n=length(x), pch=19, col="orange", cex=3, ...)
   {
     xy <- xy.coords(x, y); x <- xy$x; y <- xy$y
@@ -59,5 +65,5 @@ function(x, y=NULL, label=TRUE, n=length(x), pch=19, col="orange", cex=3, ...)
 idplot <- function(x,y,...,id=list()) {
   plot(x,y,...)
   id$x <- x; id$y <- y
-  do.call("Id",id)
+  do.call("click",id)
 }
