7692986f0ac84ad61499d9555ebf3e17b3c58e44
Klaus Holst <klaus@holst.it>
2014-04-05 09:43:30 +0000

    images function for creating image-matrices of categorical data...
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/lava@1042 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/zcolorbar.R
@@ -0,0 +1,57 @@
+##' Add color-bar to plot
+##'
+##' @title Add color-bar to plot
+##' @param clut Color look-up table
+##' @param 0.5) 
+##' @param x.range x range
+##' @param y.range y range
+##' @param values label values
+##' @param digits number of digits
+##' @param label.offset label offset
+##' @param srt rotation of labels
+##' @param cex text size
+##' @param border border of color bar rectangles
+##' @param position Label position left/bottom (1) or top/right (2) or no text (0)
+##' @param direction horizontal or vertical color bars
+##' @param ... 
+##' @param \dots additional low level arguments (i.e. parsed to \code{text})
+##' @export
+##' @examples
+##' \dontrun{
+##' plotNeuro(x,roi=R,mm=-18,range=5)
+##' colorbar(clut <- Col(rev(rainbow(11,start=0,end=0.69)),0.5),x=c(-40,40),y.range=c(84,90),values=c(-5:5))
+##' 
+##' colorbar(clut <- Col(rev(rainbow(11,start=0,end=0.69)),0.5),x=c(-10,10),y.range=c(-100,50),values=c(-5:5),direction="vertical",border=1)
+##' }
+colorbar <- function(clut=Col(rev(rainbow(11,start=0,end=0.69)),0.5),
+                     x.range=c(-.5,.5),y.range=c(-.1,.1),
+                     values=seq(clut),digits=2,label.offset,srt=45,
+                     cex=0.5,border=NA,
+                     position=1,
+                     direction=c("horizontal","vertical"),...) {
+  nlut <- length(clut)
+  X <- length(agrep(tolower(direction[1]),"horizontal"))>0
+  scale <- ifelse(X,diff(x.range),diff(y.range))/nlut
+  barsize <- ifelse(X,diff(y.range),diff(x.range))  
+  if (missing(label.offset)) label.offset <- barsize/3
+  delta <- ifelse(X,x.range[1],y.range[1])
+  if (!is.null(values)) dM <- diff(range(values))/(nlut-1)
+  for (i in seq_len(nlut+1)-1) {
+    pos <- delta + (i-1)*scale
+    if (X) {
+      x1 <- pos; x2 <- pos+scale; y1 <- y.range[1]; y2 <- y.range[2]
+    } else {
+      y1 <- pos; y2 <- pos+scale; x1 <- x.range[1]; x2 <- x.range[2]
+    }
+    if (i>0)
+      rect(x1,y1,x2,y2, col=clut[i], border=border, xpd=TRUE)
+    if (!is.null(values)) {
+      rund <- format(round(min(values)+dM*i,max(1,digits)),digits=digits)
+##      rund <- round((min(values)+dM*i)*10^digits)/(10^digits)
+      x0 <- pos+(1+0.5)*scale; y0 <- y.range[2]+label.offset
+      if (!X) { y0 <- x0; x0 <- x.range[1]-label.offset }
+      if (i<nlut)
+        text(x0,y0,rund,cex=cex,srt=srt,xpd=TRUE,...)
+    }
+  }
+}
---
+++ R/zimages.R
@@ -0,0 +1,94 @@
+img <- function(x,idx,col=list(gray.colors(10,1,0.2)),
+                ylab="Item",xlab="Subject",
+                border=1,
+                axis1=TRUE,axis2=TRUE,yaxs="r",xaxs="r",cex.axis=0.4,...) {
+    x0 <- seq(nrow(x))
+    y0 <- seq(ncol(x))
+    image(x=x0,y=y0,as.matrix(x),col=col[[1]],axes=FALSE,ylab=ylab,xlab=xlab,xaxs=xaxs,yaxs=yaxs,...)
+    if (axis1) axis(1,at=seq(nrow(x)),lwd=0.5,cex.axis=cex.axis,las=3)
+    if (axis2) axis(2,at=seq(ncol(x)),lwd=0.5,cex.axis=cex.axis,las=1)
+    if (!missing(idx)) {
+        for (i in seq_len(length(idx)))
+            image(x=idx[[i]],y=y0,as.matrix(x[idx[[i]],]),col=col[[i]],add=TRUE,xaxs=xaxs,yaxs=yaxs,...)
+    }
+}
+
+
+
+
+## X <- matrix(rbinom(400,4,0.5),20)
+## group <- rep(1:4,each=5)
+## images(X,colorbar=0)
+## images(X,group=group)
+## images(X,group=group,col=list(brewer.pal(4,"Purples"),
+##                                brewer.pal(4,"Greys"),
+##                                brewer.pal(4,"YlGn"),
+##                                brewer.pal(4,"PuBuGn")),colorbar=2)
+## images(list(X,X,X,X),group=group)
+## images(list(X,X,X,X),ncol=1,group=group)
+## images(list(X,X),group,axis2=c(FALSE,FALSE),axis1=c(FALSE,FALSE),
+##       mar=list(c(0,0,0,0),c(0,0,0,0)),yaxs="i",xaxs="i")
+
+
+
+images <- function(x,group,ncol=2,byrow=TRUE,colorbar=1,colorbar.space=0.1,label.offset=0.02,
+                 order=TRUE,colorbar.border=0,main,
+                   axis1,axis2,mar,
+                   col=list(c("#EFF3FF", "#BDD7E7", "#6BAED6", "#2171B5"),
+                       c("#FEE5D9", "#FCAE91", "#FB6A4A", "#CB181D"),
+                       c("#EDF8E9", "#BAE4B3", "#74C476", "#238B45"),
+                       c("#FEEDDE", "#FDBE85", "#FD8D3C", "#D94701")),
+                   ...) {
+    if (is.data.frame(x) || is.matrix(x)) x <- list(x)
+    K <- length(x)
+    lout <- matrix(seq(K),ncol=ncol,byrow=byrow)
+    hei <- rep(1,nrow(lout))/nrow(lout)
+    wid <- rep(1,ncol)/ncol    
+    if (colorbar==1) {
+        wid <- c(rep(1,ncol)/ncol*(1-colorbar.space),colorbar.space)
+        lout <- cbind(lout,K+1)
+    }
+    if (colorbar==2) {
+        hei <- c(rep(1,nrow(lout))/nrow(lout)*(1-colorbar.space),colorbar.space)
+        lout <- rbind(lout,K+1)
+    }
+    if (missing(group)) {
+        group <- rep(1,nrow(x[[1]]))        
+    }
+    if (missing(main)) main <- rep("",K)
+    if (!is.list(col)) col <- list(col)
+    group <- factor(group)
+    idxs <- lapply(levels(group), function(x) which(group==x))
+    layout(lout,widths=wid,heights=hei)
+    if (missing(mar)) par(mar=c(4,4,3,0))
+    if (missing(axis2)) axis2 <- c(TRUE,rep(FALSE,K-1))
+    if (missing(axis1)) axis1 <- rep(TRUE,K)
+    for (i in seq(length(x))) {
+        if (!missing(mar)) par(mar=mar[[i]])
+        img(x[[i]],idxs,col,cex.axis=0.6,axis2=axis2[i],axis1=axis1[i],main=main[i],...)
+        if (missing(mar)) par(mar=c(4,2,3,2))
+    }
+    G <- nlevels(group)
+    M <- length(col[[1]])
+    if (colorbar==1) {
+        par(mar=c(0,0,0,2))
+        plot.new(); plot.window(xlim=c(0,1),ylim=c(0,1))
+        for (i in seq(G)) {
+            guts::colorbar(col[[i]],values=seq(M)-1,direction="horizontal",
+                           y.range=c(1-i/(G+1),1-i/(G+1)+label.offset),
+                           border=colorbar.border,x.range=c(0,1),srt=0,cex=0.6)
+            text(0.5,1-i/(G+1)-label.offset, levels(group)[i])
+        }
+    }
+    if (colorbar==2) {
+        par(mar=c(0,0,0,0))
+        plot.new(); plot.window(xlim=c(0,1),ylim=c(0,1))
+        for (i in seq(G)) {
+            xr <- c(1-i/(G+1),1-i/(G+1)+.1)-.1/2
+            guts::colorbar(col[[i]],values=seq(M)-1,direction="horizontal",
+                           x.range=xr,
+                           border=colorbar.border,y.range=c(0.3,0.5),srt=0,cex=0.6)
+            text(mean(xr),.1, levels(group)[i])
+        }        
+    }
+}
