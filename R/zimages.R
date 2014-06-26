7e2d8fd12db042754b5f006574e12fbfa629ebb8
Klaus Holst <klaus@holst.it>
2014-06-26 21:02:14 +0000

    various updates to lava:::images (still not in namespace)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/lava@1171 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zimages.R
+++ R/zimages.R
@@ -1,36 +1,49 @@
 img <- function(x,idx,col=list(gray.colors(10,1,0.2)),
-                ylab="Item",xlab="Subject",
-                border=1,
+                ylab="Item",xlab="Subject",lab=TRUE,
+                border=1,rowcol=FALSE,plotfun=NULL,
                 axis1=TRUE,axis2=TRUE,yaxs="r",xaxs="r",cex.axis=0.4,...) {
     x0 <- seq(nrow(x))
     y0 <- seq(ncol(x))
     image(x=x0,y=y0,as.matrix(x),col=col[[1]],axes=FALSE,ylab=ylab,xlab=xlab,xaxs=xaxs,yaxs=yaxs,...)
-    if (axis1) axis(1,at=seq(nrow(x)),lwd=0.5,cex.axis=cex.axis,las=3)
-    if (axis2) axis(2,at=seq(ncol(x)),lwd=0.5,cex.axis=cex.axis,las=1)
+    if (axis1) {
+        axis(1,at=seq(nrow(x)),lwd=0.5,cex.axis=cex.axis,las=3)
+        if (lab) suppressWarnings(title("",xlab=xlab,...))
+    }
+    if (axis2) {
+        axis(2,at=seq(ncol(x)),lwd=0.5,cex.axis=cex.axis,las=1)
+        if (lab) suppressWarnings(title("",ylab=ylab,...))
+    }
+    if (!is.null(plotfun)) {
+        plotfun(...)
+    }
     if (!missing(idx)) {
-        for (i in seq_len(length(idx)))
-            image(x=idx[[i]],y=y0,as.matrix(x[idx[[i]],]),col=col[[i]],add=TRUE,xaxs=xaxs,yaxs=yaxs,...)
+        if (rowcol) {
+            for (i in seq_len(length(idx)))
+                image(x=x0,y=idx[[i]],as.matrix(x[,idx[[i]]]),col=col[[i]],add=TRUE,xaxs=xaxs,yaxs=yaxs,...)
+        } else 
+            for (i in seq_len(length(idx)))
+                image(x=idx[[i]],y=y0,as.matrix(x[idx[[i]],]),col=col[[i]],add=TRUE,xaxs=xaxs,yaxs=yaxs,...)
     }
 }
 
 
 
 
-## X <- matrix(rbinom(400,4,0.5),20)
+## X <- matrix(rbinom(400,3,0.5),20)
 ## group <- rep(1:4,each=5)
-## images(X,colorbar=0)
-## images(X,group=group)
+## images(X,colorbar=0,zlim=c(0,3))
+## images(X,group=group,zlim=c(0,3))
 ## images(X,group=group,col=list(brewer.pal(4,"Purples"),
 ##                                brewer.pal(4,"Greys"),
 ##                                brewer.pal(4,"YlGn"),
-##                                brewer.pal(4,"PuBuGn")),colorbar=2)
-## images(list(X,X,X,X),group=group)
-## images(list(X,X,X,X),ncol=1,group=group)
+##                                brewer.pal(4,"PuBuGn")),colorbar=2,zlim=c(0,3))
+## images(list(X,X,X,X),group=group,zlim=c(0,3))
+## images(list(X,X,X,X),ncol=1,group=group,zlim=c(0,3))
 ## images(list(X,X),group,axis2=c(FALSE,FALSE),axis1=c(FALSE,FALSE),
-##       mar=list(c(0,0,0,0),c(0,0,0,0)),yaxs="i",xaxs="i")
+##       mar=list(c(0,0,0,0),c(0,0,0,0)),yaxs="i",xaxs="i",zlim=c(0,3))
 
 images <- function(x,group,ncol=2,byrow=TRUE,colorbar=1,colorbar.space=0.1,label.offset=0.02,
-                 order=TRUE,colorbar.border=0,main,
+                 order=TRUE,colorbar.border=0,main,rowcol=FALSE,plotfun=NULL,
                    axis1,axis2,mar,
                    col=list(c("#EFF3FF", "#BDD7E7", "#6BAED6", "#2171B5"),
                        c("#FEE5D9", "#FCAE91", "#FB6A4A", "#CB181D"),
@@ -58,13 +71,13 @@ images <- function(x,group,ncol=2,byrow=TRUE,colorbar=1,colorbar.space=0.1,label
     group <- factor(group)
     idxs <- lapply(levels(group), function(x) which(group==x))
     layout(lout,widths=wid,heights=hei)
-    if (missing(mar)) par(mar=c(4,4,3,0))
+    ##if (missing(mar)) par(mar=c(4,4,3,0))
     if (missing(axis2)) axis2 <- c(TRUE,rep(FALSE,K-1))
     if (missing(axis1)) axis1 <- rep(TRUE,K)
     for (i in seq(length(x))) {
-        if (!missing(mar)) par(mar=mar[[i]])
-        img(x[[i]],idxs,col,cex.axis=0.6,axis2=axis2[i],axis1=axis1[i],main=main[i],...)
-        if (missing(mar)) par(mar=c(4,2,3,2))
+##        if (!missing(mar)) par(mar=mar[[i]])
+        img(x[[i]],idxs,col,axis2=axis2[i],axis1=axis1[i],main=main[i],rowcol=rowcol,plotfun=plotfun[[i]],...)
+##        if (missing(mar)) par(mar=c(4,2,3,2))
     }
     G <- nlevels(group)
     M <- length(col[[1]])
