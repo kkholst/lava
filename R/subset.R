6ff74c4a6d0f71090337b5683c7e706ae84c8b73
Klaus Holst <kkho@biostat.ku.dk>
2015-07-02 22:30:50 +0200

    attributes transfered to subset model (e.g. binary attributes)

--- R/subset.R
+++ R/subset.R
@@ -19,29 +19,37 @@
 ##' @export
 ##' @method subset lvm
 subset.lvm <- function(x, vars, ...) {
-  if (missing(vars))
-    return(x)
-  if (class(vars)[1]=="formula") vars <- all.vars(vars)
-  if (!all(vars%in%vars(x))) stop("Not a subset of model")
-  latentvars <- intersect(vars,latent(x))
-  ##  g0 <- subGraph(vars, Graph(x))
-  ##  res <- graph2lvm(g0)
-  res <- lvm(vars)
-  M <- t(x$M[vars,vars,drop=FALSE])
-  for (i in seq_len(nrow(M))) {
-    if (any(M[,i]==1)) {
-      res <- regression(res, y=rownames(M)[M[,i]==1], x=rownames(M)[i], ...)
+    if (missing(vars))
+        return(x)
+    if (class(vars)[1]=="formula") vars <- all.vars(vars)
+    if (!all(vars%in%vars(x))) stop("Not a subset of model")
+    latentvars <- intersect(vars,latent(x))
+    ##  g0 <- subGraph(vars, Graph(x))
+    ##  res <- graph2lvm(g0)
+    res <- lvm(vars)
+    M <- t(x$M[vars,vars,drop=FALSE])
+    for (i in seq_len(nrow(M))) {
+        if (any(M[,i]==1)) {
+            res <- regression(res, y=rownames(M)[M[,i]==1], x=rownames(M)[i], ...)
+        }
     }
-  }
-  if (length(latentvars)>0)
-    latent(res) <- latentvars
-  res$cov[vars,vars] <- x$cov[vars,vars]
-  ## Fixed parameters:
-  res$par[vars,vars] <- x$par[vars,vars]
-  res$fix[vars,vars] <- x$fix[vars,vars]
-  res$covpar[vars,vars] <- x$covpar[vars,vars]
-  res$covfix[vars,vars] <- x$covfix[vars,vars]
-  res$mean[vars] <- x$mean[vars]
-  index(res) <- reindex(res)
-  return(res)
+    if (length(latentvars)>0)
+        latent(res) <- latentvars
+    res$cov[vars,vars] <- x$cov[vars,vars]
+    ## Fixed parameters:
+    res$par[vars,vars] <- x$par[vars,vars]
+    res$fix[vars,vars] <- x$fix[vars,vars]
+    res$covpar[vars,vars] <- x$covpar[vars,vars]
+    res$covfix[vars,vars] <- x$covfix[vars,vars]
+    res$mean[vars] <- x$mean[vars]
+    res$attributes <- x$attributes
+    for (i in seq_along(x$attributes)) {
+        val <- x$attributes[[i]]
+        if (length(val)>0) {
+            val <- val[intersect(vars,names(val))]
+            res$attributes[[i]] <- val
+        }
+    }
+    index(res) <- reindex(res)
+    return(res)
 }
