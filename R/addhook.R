0bc883901814ab09a801f69935618e783ac91578
Klaus Holst <kkho@biostat.ku.dk>
2015-07-15 19:24:21 +0200

    new default itol value 1e-16

--- R/addhook.R
+++ R/addhook.R
@@ -78,7 +78,7 @@ assign("options", list(
     eval.max=250,
     constrain=FALSE,
     silent=TRUE,
-    itol=1e-12,
+    itol=1e-16,
     cluster.index=versioncheck("mets",c(0,2,7)),
     Dmethod="simple", ##Richardson"
     parallel=TRUE,
