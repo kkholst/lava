e0270812bb47e052cecb60280dfca14e22979566
Klaus Holst <kkho@biostat.ku.dk>
2016-03-14 09:42:47 +0100

    Some unused code removed and estfun optimizer updated

--- R/optims.R
+++ R/optims.R
@@ -23,158 +23,38 @@ nlminb0 <- function(start,objective,gradient,hessian,...) {
 
 ###{{{ estfun
 
-### just for testing
-estfun2 <- function(start,objective,gradient,hessian,...) {
-
-  myobj <- function(...) {
-    S <- gradient(...)
-    return(crossprod(S)[1])
-  }
-  myobj <- objective
-  g <- function(...) gradient(...)
-  h <- function(...) numDeriv::jacobian(gradient,...)
-
-  theta <- start
-  print(theta)
-  count <- 0
-  for (i in seq_len(50)) {
-    count <- count+1
-    I <- h(theta);
-    I1 <- solve(I)
-    S <- g(theta)
-    theta <- theta + (-0.5*I1%*%S)
-    print(S)
-    print(theta[,1])
-  }
-
-  return(theta)
-    res <- list(par=theta[,1], iterations=count, method="estfun0", gradient=g(start))
-
-}
-
-
-estfun <- function(start,objective,gradient,hessian,...) {
-  myobj <- function(...) {
-    S <- gradient(...)
+estfun <- function(start,objective,gradient,hessian,NR=FALSE,...) {
+  myobj <- function(x,...) {
+    S <- gradient(x,...)
     crossprod(S)[1]
   }
-  if (!is.null(hessian)) {
+  if (!missing(hessian) && !is.null(hessian)) {
     mygrad <- function(x) {
       H <- hessian(x)
       S <- gradient(x)
       2*S%*%H
     }
   } else {
-    mygrad <- function(x) {
-      myfun <- function(z) gradient(z)
-      H <- numDeriv::jacobian(myfun,x,method=lava.options()$Dmethod)
-      S <- gradient(x)
-      2*S%*%H
-    }
+      hessian <- function(x) numDeriv::jacobian(gradient,x,method=lava.options()$Dmethod)
+      mygrad <- function(x) {
+          H <- hessian(x)
+          S <- gradient(x)
+          2*S%*%H
+      }
   }
-  nlminb2(start,myobj,mygrad,hessian=NULL,...)
+  if (NR) {
+      op <- lava::NR(start,gradient=gradient,hessian=hessian,...)
+  } else {
+      op <- nlminb2(start,myobj,mygrad,hessian=NULL,...)
+  }
+  return(op)
 }
 
-estfun0 <- function(start,objective,gradient,hessian,...) {
-  myobj <- function(...) {
-    S <- gradient(...)
-    crossprod(S)[1]
-  }
-  mygrad <- function(x) {
-    myfun <- function(z) gradient(z)
-    H <- numDeriv::jacobian(myfun,x,method=lava.options()$Dmethod)
-    S <- gradient(x)
-    2*S%*%H
-  }
-  nlminb2(start,myobj,mygrad,hessian=NULL,...)
-}
+estfun0 <- function(...,hessian=NULL) estfun(...,hessian=hessian)
 
 ###}}}
 
 ###{{{ Newton-Raphson/Scoring
-
-NR0 <- function(start,objective,gradient,hessian,debug=FALSE,control,...) {
-    control0 <- list(trace=0,gamma=1,lambda=0,ngamma=0,gamma2=0,
-                     iter.max=200,tol=1e-15,stabil=FALSE,epsilon=1e-15)
-    if (!missing(control)) {
-        control0[names(control)] <- control
-    }
-    control <- control0
-    trace <- control$trace
-
-    if (trace>0)
-        cat("\nIter=0;\t\n",
-            "\tp=", paste0(formatC(start), collapse=" "),"\n")
-
-    if (missing(gradient) & missing(hessian)) {
-        hessian <- function(p) {
-            ff <- objective(p)
-            res <- attributes(ff)$hessian
-            attributes(res)$grad <- as.vector(attributes(ff)$grad)
-            return(res)
-        }
-    }
-    oneiter <- function(p.orig) {
-        if (is.null(hessian)) {
-            I <- numDeriv::jacobian(gradient,p.orig,method=lava.options()$Dmethod)
-        } else {
-            I <- hessian(p.orig)
-        }
-        D <- attributes(I)$grad
-        if (is.null(D)) {
-            D <- gradient(p.orig)
-        }
-        if (control$stabil) {
-            if (control$lambda!=0) {
-                if (control$lambda<0) {
-                    sigma <- (t(D)%*%(D))[1]
-                } else {
-                    sigma <- control$lambda
-                }
-                sigma <- min(sigma,10)
-                I <- I+control$gamma2*sigma*diag(nrow=nrow(I))
-            } else {
-                sigma <- ((D)%*%t(D))
-                I <- I+control$gamma2*(sigma)
-            }
-        }
-        svdI <- svd(I); svdI$d0 <- numeric(length(svdI$d));
-        svdI$d0[abs(svdI$d)>control$epsilon] <-
-            1/svdI$d[abs(svdI$d)>control$epsilon]
-        iI <- with(svdI,  (v)%*%diag(d0,nrow=length(d0))%*%t(u))
-        return(list(p=p.orig - control$gamma*iI%*%D,D=D,iI=iI))
-    }
-
-    count <- count2 <- 0
-    thetacur <- start
-    gammacount <- 0
-    for (jj in seq_len(control$iter.max)) {
-        gammacount <- gammacount+1
-        count <-  count+1
-        count2 <- count2+1
-        oldpar <- thetacur
-        newpar <- oneiter(thetacur)
-        thetacur <- newpar$p
-        if (!is.null(control$ngamma) && control$ngamma>0) {
-            if (control$ngamma<=gammacount) {
-                control$gamma <- sqrt(control$gamma)
-                gammacount <- 0
-            }
-        }
-        if (count2==trace) {
-            cat("Iter=",count, ";\n\tD=", paste0(formatC(newpar$D), collapse=" "),"\n")
-            cat("\tp=", paste0(formatC(thetacur), collapse=" "),"\n")
-            count2 <- 0
-        }
-        if (mean(newpar$D^2)<control$tol) break;
-    }
-    res <- list(par=as.vector(thetacur), iterations=count, method="NR", gradient=newpar$D, iH=newpar$iI)
-    return(res)
-}
-
-###}}} NR
-
-###{{{ NR 2
 
 ##' @export
 NR <- function(start,objective,gradient,hessian,debug=FALSE,control,...) {
