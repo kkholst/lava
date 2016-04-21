c0cf23be300379ea40f7b8037f981e08c41fe23d
Klaus Holst <kkho@biostat.ku.dk>
2016-04-21 16:00:44 +0200

    preserve names of vector/list/matrix

--- R/vec.R
+++ R/vec.R
@@ -9,8 +9,18 @@
 ##' @author Klaus Holst
 ##' @export
 vec <- function(x,matrix=FALSE,sep=".",...) {
-    nn <- apply(expand.grid(dimnames(x)),1,function(x) paste(x,collapse=sep))
-    res <- as.vector(x); names(res) <- nn
+    if (is.vector(x) && !is.list(x)) {
+        res <- x
+    } else if (is.list(x)) {
+        res <- setNames(unlist(x),names(x))
+    } else {
+        if (is.matrix(x) && is.null(rownames(x))) {
+            nn <- colnames(x)
+        } else {
+            nn <- apply(expand.grid(dimnames(x)),1,function(x) paste(x,collapse=sep))
+        }        
+        res <- as.vector(x); names(res) <- nn
+    }
     if (matrix) return(cbind(res))
     return(res)
 }
