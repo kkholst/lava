aa992c41a66b3425b507cc27f6b649252b4f0e8c
Klaus Holst <kkho@biostat.ku.dk>
2017-03-09 19:30:28 +0100

    non-difference contrast with regex/wildcards (diff argument)

--- R/contr.R
+++ R/contr.R
@@ -14,21 +14,23 @@
 ##' contr(c(2,3,4),n=5)
 ##' contr(list(c(1,3),c(2,4)),n=5)
 ##' contr(list(c(1,3),c(2,4),5))
+##'
+##' parsedesign(c("aa","b","c"),"?","?",diff=c(FALSE,TRUE))
 contr <- function(p,n,diff=TRUE,...) {
     if (missing(n)) n <- max(unlist(p))
     if (is.character(p)) {
         return(parsedesign(n,p,...))
     }
     if (is.list(p)) {
-        return(Reduce(rbind,lapply(p, function(x) do.call(contr, list(x,n,diff)))))
+        return(Reduce(rbind,lapply(p, function(x) do.call(contr, list(x,n,diff[1L])))))
     }
     if (is.character(n)) n <- length(n)
     if (!is.numeric(n)) {
         try(n <- length(coef(n)),silent=TRUE)
     }
-    B <- matrix(0,ncol=n,nrow=max(1,length(p)-1))
-    B[,p[1]] <- 1
-    if (length(p)>1)
-        B[cbind(seq(nrow(B)),p[-1])] <- ifelse(diff,-1,1)
+    B <- matrix(0,ncol=n,nrow=max(1L,length(p)-1L))
+    B[,p[1]] <- 1L
+    if (length(p)>1L)
+        B[cbind(seq(nrow(B)),p[-1])] <- ifelse(diff[1L],-1,1)
     return(B)
 }
