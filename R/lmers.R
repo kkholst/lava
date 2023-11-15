e4a0981dba4404e06245348bb77189a25a62ad1c
Klaus Holst <klaus@holst.it>
2023-11-15 22:09:19 +0100

    clean-up

--- R/lmers.R
+++ R/lmers.R
@@ -1,27 +1,33 @@
 ##v <- lmerplot(l1,varcomp=TRUE,colorkey=TRUE,lwd=0,col=rainbow(20))
 lmerplot <- function(model,x,id,y,transform,re.form=NULL,varcomp=FALSE,colorbar=TRUE,mar=c(4,4,4,6),col,index=seq(50),...) {
     if (varcomp) {
-        Z <- lme4::getME(model,"Z")
-        nn <- unlist(lapply(lme4::getME(model,"Ztlist"),nrow))
-        ve <- lme4::getME(model,"sigma")^2
+        Z <- lme4::getME(model, "Z")
+        nn <- unlist(lapply(lme4::getME(model, "Ztlist"), nrow))
+        ve <- lme4::getME(model, "sigma")^2
         vu <- varcomp(model,profile=FALSE)$varcomp
-        L <- Matrix::Diagonal(sum(nn),rep(vu,nn))
+        L <- Matrix::Diagonal(sum(nn), rep(vu, nn))
         V <- Z%*%L%*%(Matrix::t(Z))
         Matrix::diag(V) <- Matrix::diag(V)+ve
         cV <- Matrix::cov2cor(V)
         if (!is.null(index)) {
-            index <- intersect(seq(nrow(cV)),index)
+            index <- intersect(seq_len(nrow(cV)), index)
             cV <- cV[index,index,drop=FALSE]
         }
         if (colorbar) { opt <- par(mar=mar) }
         if (missing(col)) col <- rev(gray.colors(16,0,1))
-        image(seq(nrow(cV)),seq(ncol(cV)),as.matrix(cV),xlab="",ylab="",col=col,zlim=c(0,1),...)
+        image(seq_len(nrow(cV)), seq_len(ncol(cV)),
+              as.matrix(cV),
+            xlab = "", ylab = "", col = col, zlim = c(0, 1), ...
+        )
         if (colorbar) {
             uu <- devcoords()
             xrange <- c(uu$fig.x2,uu$dev.x2)
             xrange <- diff(xrange)/3*c(1,-1)+xrange
             yrange <- c(uu$fig.y1,uu$fig.y2)
-            colorbar(direction="vertical",x.range=xrange,y.range=yrange,clut=col,values=seq(0,1,length.out=length(col)),srt=0,position=2)
+            colorbar(
+              direction = "vertical", x.range = xrange, y.range = yrange,
+              clut = col, values = seq(0, 1, length.out = length(col)), srt = 0, position = 2
+            )
             par(opt)
         }
         return(invisible(V))
@@ -34,8 +40,8 @@ lmerplot <- function(model,x,id,y,transform,re.form=NULL,varcomp=FALSE,colorbar=
         if (!is.null(re.form)) ymean <- transform(ymean)
         y <- transform(y)
     }
-    plot(y ~ x, col=Col(id,0.3), pch=16,...)
-    if (!is.null(re.form)) points(ymean ~ x, pch="-",cex=4);
+    plot(y ~ x, col=Col(id,0.3), pch=16, ...)
+    if (!is.null(re.form)) points(ymean ~ x, pch="-", cex=4);
     for (i in unique(id)) {
         idx <- which(id==i)
         lines(yhat[idx]~x[idx],col=i)
