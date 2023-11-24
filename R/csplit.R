b986c9a006a9c9d60d58bfda1fb3cafd66283a34
Klaus Holst <klaus@holst.it>
2023-11-24 23:50:03 +0100

    foldr with list argument

--- R/csplit.R
+++ R/csplit.R
@@ -15,30 +15,46 @@
 ##' csplit(iris[1:10,]) ## Split in two sets 1:(n/2) and (n/2+1):n
 ##' csplit(iris[1:10,],0.5)
 ##' @author Klaus K. Holst
-csplit <- function(x,p=NULL,replace=FALSE,return.index=FALSE,k=2,...) {
-    if (length(x)==1 & is.numeric(x)) x <- seq(x)
-    N <- NROW(x)
-    if (is.null(p)) { ##
-        K <- base::round(N/k)
-        idx <- split(seq(N),sort(rep(seq(k),length.out=N,each=K)))
-    } else {
-        if (p<1) { ## two folds (size N*p and N*(1-p))
-            idx1 <- base::sample(N,base::round(p*N),replace=replace)
-            idx <- list(idx1,
-                        base::sample(setdiff(seq(N),idx1),replace=replace))
-        } else { ## Number of folds (equal size)
-            idx <- split(sample(seq(N)), rep(seq(p), length=N))
-        }
+csplit <- function(x, p=NULL, replace=FALSE,
+                   return.index=FALSE, k=2, ...) {
+  if (length(x)==1 && is.numeric(x))
+    x <- seq(x)
+  N <- NROW(x)
+  if (is.null(p)) { ##
+    K <- base::round(N/k)
+    idx <- split(seq(N), sort(rep(seq(k), length.out=N, each=K)))
+  } else {
+    if (p<1) { ## two folds (size N*p and N*(1-p))
+
+      idx1 <- base::sample(N, base::round(p*N), replace=replace)
+      idx <- list(idx1,
+                  base::sample(setdiff(seq(N), idx1), replace=replace))
+    } else { ## Number of folds (equal size)
+      idx <- split(sample(seq(N)), rep(seq(p), length=N))
     }
-    if (return.index)
-        return(idx)
-    if (!is.vector(x)) {
-        return(lapply(idx,function(ii) x[ii,,drop=FALSE]))
-    }
-    return(lapply(idx,function(ii) x[ii]))
+  }
+  if (return.index)
+    return(idx)
+  if (!is.vector(x)) {
+    return(lapply(idx, function(ii) x[ii, , drop=FALSE]))
+  }
+  return(lapply(idx, function(ii) x[ii]))
 }
 
 ##' @export
-foldr <- function(n,K=5,rep=10) {
-    replicate(rep,split(sample(seq(n)), rep(seq(K), length=n)),simplify=FALSE)
+foldr <- function(n, K=5, rep=1, list=TRUE) {
+  res <- replicate(rep, split(sample(seq(n)),
+                              rep(seq(K), length=n)),
+                   simplify=FALSE)
+  if (!list) {
+    ids <- rep(seq_len(length(res[[1]])),
+           unlist(lapply(res[[1]], length)))
+    res <- lapply(res, function(x) {
+      ids[order(unlist(x))]
+    })
+    if (length(res)==1)
+      res <- res[[1]]
+  }
+  return(res)
 }
+
