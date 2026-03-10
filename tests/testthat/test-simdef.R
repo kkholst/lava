bf9162971fd9ec1b564dceac0e3612c0591065e2
Klaus Holst <klaus@holst.it>
2026-03-10 17:07:24 +0100

    unit-tests

--- tests/testthat/test-simdef.R
+++ tests/testthat/test-simdef.R
@@ -29,3 +29,30 @@ test_that("sim.default with estimate objects", {
   expect_equivalent(s["SE",], colMeans(res[, c("Std.Err.a", "Std.Err.b")]))
   expect_equivalent(s["SD",], c(sd(res[,"a"]), sd(res[,"b"])))
 })
+
+test_that("sim.default subsets", {
+  onerun <- function(...) estimate(coef=runif(2),
+                                   vcov=diag(runif(2)),
+                                   labels=c("a","b"))
+  res <- sim(onerun, 100, estimate.index = 1)
+
+  expect_true(ncol(res) == 2L)
+  expect_true(nrow(res) == 100L)
+
+  expect_true(nrow(
+    rbind(res, res)
+  ) == 200L)
+
+  expect_true(nrow(
+    rbind(res[1:20, 1], res[1:50, 1])
+  ) == 70L)
+
+  expect_true(ncol(
+    cbind(res[1:50, 1], res[1:50, 2])
+  ) == 2L)
+
+  expect_true(ncol(
+    cbind(res[1:50, 1:2], res[1:50, 1:2])
+  ) == 4L)
+
+})
