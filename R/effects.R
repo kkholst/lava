d65fd5dc01518569c207e40f0079b0703583e180
Klaus Holst <klaus@holst.it>
2026-06-26 09:35:29 +0200

    using summary.estimate instead of back.transform

--- R/effects.R
+++ R/effects.R
@@ -161,10 +161,9 @@ summary.effects <- function(object,...) {
     medprop <- NULL
     if (totindirectef[1]!=0) {
         if (abs(coef(object)[2])<1e-12) {
-            medprop <- estimate(NULL,coef=c("Mediation proportion"=1),vcov=matrix(NA))
+          medprop <- estimate(NULL,coef=c("Mediation proportion"=1),vcov=matrix(NA))
         } else {
-            medprop <- estimate(object, function(x) list("Mediation proportion"=1-x[2]/x[1]))
-            ##medprop <- estimate(object, function(x) list("Mediation proportion"=logit(x[3]/x[1])),back.transform=expit)
+          medprop <- estimate(object, function(x) list("Mediation proportion"=1-x[2]/x[1]))
         }
     }
     list(coef=mycoef,medprop=medprop)
